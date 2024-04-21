925a6537a1e1c58c673834e748e140de86419e2f
megha <meghakundu808@gmail.com>
2024-04-21 17:43:46 +0530

    updated headings for csv export file

--- app/Exports/ContactsExport.php
+++ app/Exports/ContactsExport.php
@@ -4,11 +4,19 @@ namespace App\Exports;
 
 use App\Models\Contact;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ContactsExport implements FromCollection
+class ContactsExport implements FromCollection,WithHeadings
 {
     public function collection()
     {
-        return Contact::where("user_id", auth()->id())->get(["id", "name", "email", "phone", "address","category_id","created_at","user_id"]);
+        return Contact::join('categories','contacts.category_id','categories.id')
+                      ->join('users','contacts.user_id','users.id')->where("contacts.user_id", auth()->id())->get(["contacts.id","contacts.name as contact","contacts.email", "contacts.phone", "contacts.address","categories.name as category","contacts.created_at","users.name"]);
+    }
+    public function headings(): array
+    {
+        return [
+            'id','name','email','phone', 'address','category','created_at','added_by'
+        ];
     }
 }
